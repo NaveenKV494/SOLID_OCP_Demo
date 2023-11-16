2485242f82f5d2ea40fc8e96b23766a8e21e8f5f
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-16 15:12:58 +0530

    added the property to Person model

--- SOLID_OCP_Demo/PersonModel.cs
+++ SOLID_OCP_Demo/PersonModel.cs
@@ -10,5 +10,6 @@ namespace SOLID_OCP_Demo
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public EmployeeType TypeOfEmployee { get; set; }
     }
 }
