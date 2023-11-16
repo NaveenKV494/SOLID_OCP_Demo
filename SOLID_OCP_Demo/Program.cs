32b3589f49a5f75d716a5ddfe9fea98d10bf4610
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-16 14:49:16 +0530

    added the list of persons, employees and Accounts

--- SOLID_OCP_Demo/Program.cs
+++ SOLID_OCP_Demo/Program.cs
@@ -10,6 +10,27 @@ namespace SOLID_OCP_Demo
     {
         static void Main(string[] args)
         {
+            List<PersonModel> applicants = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Naveen", LastName = "Kumar" },
+                new PersonModel { FirstName = "Karthik", LastName = "Vijay" },
+                new PersonModel { FirstName = "Selva", LastName = "Nathan" }
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+
+            foreach (var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{emp.FirstName} {emp.LastName}: {emp.EmailAddress} IsManager: {emp.IsManager} IsExecutive: {emp.IsExecutive}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
